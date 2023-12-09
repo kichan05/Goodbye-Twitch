60fc6a25599d88a7e0da69434087b20591146f47
바키찬 <ckstmznf@naver.com>
2023-12-10 04:04:30 +0900

    타이머 완성

--- src/App.js
+++ src/App.js
@@ -1,12 +1,60 @@
 import './App.css';
-import {useEffect, useState} from "react";
+import {useEffect, useMemo, useRef, useState} from "react";
 import moment from 'moment';
+
+const Number = ({number, row, is}) => {
+  const element = useRef()
+  const top = useMemo(() => {
+    if(element.current === undefined) return 0
+    return element.current.offsetHeight * number * -1
+  }, [number])
+
+  let left
+  if(element.current !== undefined)
+    left = element.current.offsetWidth * row
+
+  return (
+    <div className="number-wrap" style={{top, left}}>
+      <div
+        ref={element}
+        className="0">0</div>
+      <div
+        className="1">1</div>
+      <div
+        className="2">2</div>
+      <div
+        className="3">3</div>
+      <div
+        className="4">4</div>
+      <div
+        className="5">5</div>
+      <div
+        className="6">6</div>
+      <div
+        className="7">7</div>
+      <div
+        className="8">8</div>
+      <div
+        className="9">9</div>
+    </div>
+  )
+}
 
 function App() {
   const END_D_DAY = moment("2024-02-27:00:00:00")
   const [d_day, setDDay] = useState({
-    days : 0, hours : 0, minutes : 0, second : 0
+    days: 0, hours: 0, minutes: 0, second: 0, asSecond : 0
   })
+
+  function getNumberArray(number) {
+    let result = []
+    while (number > 0){
+      result.push(number % 10)
+      number = Math.floor(number / 10)
+    }
+    result.reverse()
+    return result
+  }
 
   useEffect(() => {
     const timeInterval = setInterval(() => {
@@ -14,13 +62,12 @@ function App() {
       const duration = moment.duration(d)
 
       setDDay({
-        days : Math.floor(duration.asDays()),
+        days: Math.floor(duration.asDays()),
         hours: duration.hours(),
         minutes: duration.minutes(),
         second: duration.seconds(),
+        asSecond : Math.floor(duration.asSeconds()),
       })
-
-      console.log(d_day)
     }, 500)
 
     return () => {
@@ -30,7 +77,17 @@ function App() {
 
   return (
     <div className="App">
-      {d_day.days}일 {d_day.hours}시간 {d_day.minutes}분 {d_day.second}초
+      <div className="content">
+        <h1>트위치 종료까지</h1>
+        <div className="d-day">
+          {
+            getNumberArray(d_day.asSecond).map((i, index) => {
+              return <Number key={index} number={i} row={index} is={d_day.asSecond}/>
+            })
+          }
+        </div>
+        <h1>초 남았습니다.</h1>
+      </div>
     </div>
   );
 }
