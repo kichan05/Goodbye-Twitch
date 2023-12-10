769441d0ebe1a8b4e8994102b2777b18da44d3a3
바키찬 <ckstmznf@naver.com>
2023-12-10 22:45:40 +0900

    fix:ios에서 시간 안보이던 이슈 수정

--- src/App.js
+++ src/App.js
@@ -1,49 +1,39 @@
 import './App.css';
-import {useEffect, useMemo, useRef, useState} from "react";
+import {useEffect, useState, useRef} from "react";
 import moment from 'moment';
+import { DateTime } from 'luxon'
 
-const Number = ({number, row}) => {
+const Number = ({number, row, currentTime}) => {
   const element = useRef()
-  const [top, setTop] = useState(0)
+  const [top, setTop] = useState(62 * number * -1)
 
   useEffect(() => {
-    if(element.current === undefined) return
     setTop(element.current.offsetHeight * number * -1)
-  }, )
-
-  let left
-  if(element.current !== undefined)
-    left = element.current.offsetWidth * row
+  }, [number, currentTime])
 
   return (
-    <div className="number-wrap" style={{top, left}}>
+    <div
+      className={`number-wrap ${number}`}
+      style={{transform: `translateY(${top}px)`}}
+    >
       <div
         ref={element}
         className="0">0</div>
-      <div
-        className="1">1</div>
-      <div
-        className="2">2</div>
-      <div
-        className="3">3</div>
-      <div
-        className="4">4</div>
-      <div
-        className="5">5</div>
-      <div
-        className="6">6</div>
-      <div
-        className="7">7</div>
-      <div
-        className="8">8</div>
-      <div
-        className="9">9</div>
+      <div className="1">1</div>
+      <div className="2">2</div>
+      <div className="3">3</div>
+      <div className="4">4</div>
+      <div className="5">5</div>
+      <div className="6">6</div>
+      <div className="7">7</div>
+      <div className="8">8</div>
+      <div className="9">9</div>
     </div>
   )
 }
 
 function App() {
-  const END_D_DAY = moment("2024-02-27:00:00:00")
+  const END_D_DAY = DateTime.fromObject({ year: 2024, month: 2, day: 27 });
   const [d_day, setDDay] = useState({
     days: 0, hours: 0, minutes: 0, second: 0, asSecond : 0
   })
@@ -60,15 +50,14 @@ function App() {
 
   useEffect(() => {
     const timeInterval = setInterval(() => {
-      const d = END_D_DAY.diff(moment())
-      const duration = moment.duration(d)
+      const duration = END_D_DAY.diff(DateTime.now(), ['days', 'hours', 'minutes', 'seconds'])
 
       setDDay({
-        days: Math.floor(duration.asDays()),
-        hours: duration.hours(),
-        minutes: duration.minutes(),
-        second: duration.seconds(),
-        asSecond : Math.floor(duration.asSeconds()),
+        days: duration.days,
+        hours: duration.hours,
+        minutes: duration.minutes,
+        second: Math.floor(duration.seconds),
+        asSecond : Math.floor(duration.as("seconds")),
       })
     }, 500)
 
@@ -79,13 +68,14 @@ function App() {
 
   return (
     <div className="App">
+      {d_day.days} {d_day.hours} {d_day.days} {d_day.hours} {d_day.minutes} {d_day.second} {d_day.asSecond}
       <div className="content">
         <h1>트위치 종료까지</h1>
         <div className="d-day">
           {
-            getNumberArray(d_day.asSecond).map((i, index) => {
-              return <Number key={index} number={i} row={index} is={d_day.asSecond}/>
-            })
+            getNumberArray(d_day.asSecond).map((i, index) => (
+              <Number key={index} number={i} row={index} currentTime={d_day.second}/>
+            ))
           }
         </div>
         <h1>초 남았습니다.</h1>
---
+++ src/component/Number.js
@@ -0,0 +1,4 @@
+import {useEffect, useRef, useState} from "react";
+
+
+// export default Number
